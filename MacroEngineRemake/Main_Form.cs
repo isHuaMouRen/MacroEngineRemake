5e333f9c729ab43e83d995764a7b9543e38207b0
某人 华 <2825338723@qq.com>
2025-10-07 10:18:58 +0800

    添加项目文件。

---
+++ MacroEngineRemake/Main_Form.cs
@@ -0,0 +1,38 @@
+﻿using ToolLib.AreaSelectorLib;
+using ToolLib.ErrorReportLib;
+using ToolLib.PosSelectorLib;
+using ToolLib.AutoStartLib;
+using ToolLib.CmdLib;
+using ToolLib.DownloaderLib;
+using ToolLib.GdiToolLib;
+using ToolLib.HashLib;
+using ToolLib.HexLib;
+using ToolLib.HotkeyManagerLib;
+using ToolLib.IniLib;
+using ToolLib.InputLib;
+using ToolLib.JsonLib;
+using ToolLib.KeyboardHookLib;
+using ToolLib.LogLib;
+using ToolLib.MemoryLib;
+using ToolLib.RegistryLib;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MacroEngineRemake
+{
+    public partial class Main_Form : Form
+    {
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MacroEngineRemake/Program.cs
@@ -0,0 +1,54 @@
+﻿using ToolLib.AreaSelectorLib;
+using ToolLib.ErrorReportLib;
+using ToolLib.PosSelectorLib;
+using ToolLib.AutoStartLib;
+using ToolLib.CmdLib;
+using ToolLib.DownloaderLib;
+using ToolLib.GdiToolLib;
+using ToolLib.HashLib;
+using ToolLib.HexLib;
+using ToolLib.HotkeyManagerLib;
+using ToolLib.IniLib;
+using ToolLib.InputLib;
+using ToolLib.JsonLib;
+using ToolLib.KeyboardHookLib;
+using ToolLib.LogLib;
+using ToolLib.MemoryLib;
+using ToolLib.RegistryLib;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MacroEngineRemake
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+            Application.ThreadException += (sender, e) => GlobalExceptionHandler(sender, e.Exception);
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) => GlobalExceptionHandler(sender, e.ExceptionObject as Exception);
+            TaskScheduler.UnobservedTaskException += (sender, e) => GlobalExceptionHandler(sender, e.Exception);
+
+            try { Application.Run(new Form()); } catch (Exception ex) { GlobalExceptionHandler(null, ex); }
+
+
+        }
+
+        private static void GlobalExceptionHandler(object sender, Exception ex)
+        {
+            if (ErrorReportBox.Show("Error", "未捕获的异常", ex) == DialogResult.Abort) Environment.Exit(1);
+        }
+    }
+}
